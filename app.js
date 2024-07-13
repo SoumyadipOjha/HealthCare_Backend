28bcf13d06186e1870239d8ceca2c608eb99035c
Soumyadip Ojha <soumyadipojha635@gmail.com>
2024-07-13 23:23:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { dbConnection } from "./Database/dbConnection.js";
+import { errorMiddleware } from "./middlewares/error.js";
+import userRouter from "./routes/userRouter.js";
+import fileUpload from "express-fileupload";
+import ItemRouter from "./routes/ItemRouter.js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(_dirname,"./config/config.env") });
+console.log(process.env.MONGO_URL);
+const app = express();
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "PUT", "DELETE", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/post", ItemRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
