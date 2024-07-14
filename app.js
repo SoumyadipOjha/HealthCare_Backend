110d8053edc975c09738627a51aeafb10fd1bbfb
Soumyadip Ojha <soumyadipojha635@gmail.com>
2024-07-14 21:50:06 +0530

    important

--- app.js
+++ app.js
@@ -7,17 +7,24 @@ import { errorMiddleware } from "./middlewares/error.js";
 import userRouter from "./routes/userRouter.js";
 import fileUpload from "express-fileupload";
 import ItemRouter from "./routes/ItemRouter.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const _dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(_dirname,"./config/config.env") });
+dotenv.config({ path: path.resolve(_dirname, "./config/config.env") });
 console.log(process.env.MONGO_URL);
 const app = express();
-app.use(
-  cors()
-);
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(cookieParser());
 app.use(express.json());
